c45e0b2138eb14d2854e0c6bf7483e63a2d486d4
Maximilien-Hantonne <comelede@gmail.com>
2024-12-15 00:39:15 +0100

    main function to test operator and hamiltonian

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
-#include<chrono>
-#include<random>
-#include<fstream>
-#include<iostream>
-#include<vector>
-#include<complex>
-#include<array>
-#include<cmath>
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+#include <Eigen/Eigenvalues>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
 
+#include "hamiltonian.h"
+#include "operator.h"
 
-#include<Eigen> 
-// #include<xdiag>
+int main() {
+	int m = 4;
+	int n = 4;
 
+	double J = 1;
+	double U = 0;
+	double mu = 1;
 
-#include"lattice.h"
+	std::vector < std::vector<int>> neighbours = {{1,3}, {0,2}, {1,3},{2,0}};
 
+	Hamiltonian hmatrix (neighbours, m, n, J, U, mu);
 
-int main()
-{
+	Eigen::SparseMatrix<double> smatrix = hmatrix.getHamiltonian();
 
-    return 0; 
+	Operator H(std::move(smatrix));
+	Eigen::VectorXd v_0 = Eigen::VectorXd::Random(H.size());
+	Eigen::MatrixXd eigenvectors;
+	Eigen::MatrixXd V;
+	Eigen::VectorXd eigenvalues = H.lanczos_eigen(10, v_0, V, eigenvectors);
+
+	std::cout << eigenvalues.transpose() << std::endl;
+
+	return 0;
 }
